980b49094434f49748de9158c9d22e35f5459098
Dhairya Chheda <dhairyakchheda@gmail.com>
2022-10-31 20:13:56 +0530

    Toggle task reminder

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import { useState } from "react";
-import Header from './components/Header';
-import Tasks from './components/Tasks';
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
 
 function App() {
-
   const [tasks, setTasks] = useState([
     {
       id: 1,
@@ -28,13 +27,25 @@ function App() {
   // Delete task
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id));
-  }
+  };
+
+  // Toggle reminder
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
+  };
 
   return (
     <div className="container">
-      <Header/>
-      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask}/> : 'No Tasks To Show'}
-      
+      <Header />
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+      ) : (
+        "No Tasks To Show"
+      )}
     </div>
   );
 }
